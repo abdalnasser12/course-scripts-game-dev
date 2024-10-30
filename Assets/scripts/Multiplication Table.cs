a0cea44decbba01e2d5d24b3958b94efca0823ff
abdalnasser12 <140701853+abdalnasser12@users.noreply.github.com>
2024-10-30 19:09:01 +0200

    Multiplication Table for 5 ++

---
+++ Assets/scripts/Multiplication Table.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       for(int i = 1 ; i <= 10 ; i++ )
+       {
+       int result = Multiply(i,5);
+        Debug.Log(i + "* 5="+ result  );
+       }
+    }
+int Multiply (int number,int number1)
+{
+    int cross = number * number1 ;
+    return cross;
+}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
