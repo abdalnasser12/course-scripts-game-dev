e256833856b48540842402d7eda72de551a8b772
abdalnasser12 <140701853+abdalnasser12@users.noreply.github.com>
2024-11-09 22:35:28 +0200

    name spaces

--- Assets/scripts/lecture 8/GameManager.cs
+++ Assets/scripts/lecture 8/GameManager.cs
@@ -1,30 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameManager : MonoBehaviour
+namespace lecture8
 {
+
+
+  public class GameManager : MonoBehaviour
+  {
     // Start is called before the first frame update
     void Start()
     {
-        Player abdalnasser = new Player();
-      abdalnasser.InilializePlayer("abdalnasser",50) ;
-        abdalnasser.Heal(50);
-        abdalnasser.Heal(true);
-        abdalnasser.ShowCount();
-        Player ahmad = new Player();
-        ahmad.InilializePlayer("ahmed", 30);
-        ahmad.Heal(50);
-        ahmad.Heal(false);
-        ahmad.ShowCount();
+      Player abdalnasser = new Player();
+      abdalnasser.InilializePlayer("abdalnasser", 50);
+      abdalnasser.Heal(50);
+      abdalnasser.Heal(true);
+      abdalnasser.ShowCount();
+      Player ahmad = new Player();
+      ahmad.InilializePlayer("ahmed", 30);
+      ahmad.Heal(50);
+      ahmad.Heal(false);
+      ahmad.ShowCount();
 
-       
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+  }
 }
