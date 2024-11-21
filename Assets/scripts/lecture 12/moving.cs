54fc31b97ed7df1342ade59e6be897e46856a8a3
abdalnasser12 <140701853+abdalnasser12@users.noreply.github.com>
2024-11-21 20:06:41 +0200

    ++

--- Assets/scripts/lecture 12/moving.cs
+++ Assets/scripts/lecture 12/moving.cs
@@ -6,31 +6,33 @@ using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
 using UnityEngine.UIElements;
-
-public class moving : MonoBehaviour
+namespace lecture12
 {
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class moving : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //"Horizontal"
-        //"Vertical"
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
 
-        transform.position += new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+        }
 
-       
+        // Update is called once per frame
+        void Update()
+        {
+            //"Horizontal"
+            //"Vertical"
 
-
-
-
+            transform.position += new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
 
 
 
+
+
+
+
+
+
+        }
     }
 }
