567743e242d1a1bde9a24eededd48808a2c83efd
abdalnasser12 <140701853+abdalnasser12@users.noreply.github.com>
2024-11-21 20:05:16 +0200

    ++

--- Assets/scripts/lecture 12/shoot.cs
+++ Assets/scripts/lecture 12/shoot.cs
@@ -3,37 +3,39 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 
-
-public class shoot : MonoBehaviour
+namespace lecture12
 {
-    public float deg = 0f;
-    public float angleInc = 1f;
-    int Score = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class shoot : MonoBehaviour
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-
-        if (Input.GetKeyDown(KeyCode.F))
+        public float deg = 0f;
+        public float angleInc = 1f;
+        int Score = 0;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            deg = deg + angleInc;
-            transform.position += new Vector3(0, 0, -0.5f);
-            transform.rotation = Quaternion.Euler(new Vector3(deg, 0, 0));
-        }
-        if (transform.position.z <= -4.0f)
-        {
-            Score++;
-            Debug.Log("0 : " + Score);
-            transform.position = new Vector3(-0.00257609994f, 0.231999993f, -0.00999999978f);
-
 
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                deg = deg + angleInc;
+                transform.position += new Vector3(0, 0, -0.5f);
+                transform.rotation = Quaternion.Euler(new Vector3(deg, 0, 0));
+            }
+            if (transform.position.z <= -4.0f)
+            {
+                Score++;
+                Debug.Log("0 : " + Score);
+                transform.position = new Vector3(-0.00257609994f, 0.231999993f, -0.00999999978f);
+
+
+            }
+
+        }
     }
 }
