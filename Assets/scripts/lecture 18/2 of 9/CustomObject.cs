bd19a41414d4484fdf929d141baf05e6a13b9b65
abdalnasser12 <140701853+abdalnasser12@users.noreply.github.com>
2024-12-29 21:20:52 +0200

    ++

--- Assets/scripts/lecture 18/2 of 9/CustomObject.cs
+++ Assets/scripts/lecture 18/2 of 9/CustomObject.cs
@@ -1,69 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace lecture18
 {
     public class CustomObject
     {
-        private int _id;
-        private string _name;
 
-        public int Id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
 
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
 
         public CustomObject(int id, string name)
         {
-            Id = id;
+            ID = id;
             Name = name;
         }
 
         public override string ToString()
         {
-            return $"ID: {Id}, Name: {Name}";
+            return $"Object [ID: {ID}, Name: {Name}]";
         }
+
+
+
 
         public override bool Equals(object obj)
         {
-            if (obj == null || GetType() != obj.GetType())
+            if (obj is CustomObject other)
             {
-                return false;
+                return this.ID == other.ID && this.Name == other.Name;
             }
-            CustomObject other = (CustomObject)obj;
-            return this.Id == other.Id && this.Name == other.Name;
+            return false;
         }
 
-        public override int GetHashCode()
-        {
-            return Id.GetHashCode() ^ Name.GetHashCode();
-        }
+
+
 
         public static bool operator ==(CustomObject obj1, CustomObject obj2)
         {
-
-            if (obj1 == null && obj2 == null)
-                return true;
-
-
-            if (obj1 == null || obj2 == null)
-                return false;
-
-
-            return obj1.Id == obj2.Id && obj1.Name == obj2.Name;
+            return obj1.Equals(obj2);
         }
 
         public static bool operator !=(CustomObject obj1, CustomObject obj2)
         {
-            return !(obj1 == obj2);
+            return !obj1.Equals(obj2);
         }
 
     }
+
 }
+
