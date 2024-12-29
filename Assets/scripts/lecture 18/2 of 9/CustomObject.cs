87a03630cc291d5ea1159954c181388ecf9f47ec
abdalnasser12 <140701853+abdalnasser12@users.noreply.github.com>
2024-12-29 19:13:32 +0200

    ++

--- Assets/scripts/lecture 18/2 of 9/CustomObject.cs
+++ Assets/scripts/lecture 18/2 of 9/CustomObject.cs
@@ -7,26 +7,30 @@ namespace lecture18
     {
         private int _id;
         private string _name;
-        public int iid
+
+        public int Id
         {
             get { return _id; }
             set { _id = value; }
-
         }
-        public string nname
+
+        public string Name
         {
             get { return _name; }
             set { _name = value; }
         }
+
         public CustomObject(int id, string name)
         {
-            iid = id;
-            nname = name;
+            Id = id;
+            Name = name;
         }
+
         public override string ToString()
         {
-            return $"ID:{nname}, Name: {iid}";
+            return $"ID: {Id}, Name: {Name}";
         }
+
         public override bool Equals(object obj)
         {
             if (obj == null || GetType() != obj.GetType())
@@ -34,17 +38,26 @@ namespace lecture18
                 return false;
             }
             CustomObject other = (CustomObject)obj;
-            return this.iid == other.iid && this.nname == other.nname;
-
+            return this.Id == other.Id && this.Name == other.Name;
         }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^ Name.GetHashCode();
+        }
+
         public static bool operator ==(CustomObject obj1, CustomObject obj2)
         {
-            if (ReferenceEquals(obj1, null) && ReferenceEquals(obj2, null))
+
+            if (obj1 == null && obj2 == null)
                 return true;
 
-            if (ReferenceEquals(obj1, null) || ReferenceEquals(obj2, null))
+
+            if (obj1 == null || obj2 == null)
                 return false;
-            return obj1.iid == obj2.iid && obj1.nname == obj2.nname;
+
+
+            return obj1.Id == obj2.Id && obj1.Name == obj2.Name;
         }
 
         public static bool operator !=(CustomObject obj1, CustomObject obj2)
