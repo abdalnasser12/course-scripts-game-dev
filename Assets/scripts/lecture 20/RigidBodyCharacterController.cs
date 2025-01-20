eec1a1f526e50b96f2d683e742d9258cfe6b9d29
abdalnasser12 <140701853+abdalnasser12@users.noreply.github.com>
2025-01-20 18:11:30 +0200

    ++

--- Assets/scripts/lecture 20/RigidBodyCharacterController.cs
+++ Assets/scripts/lecture 20/RigidBodyCharacterController.cs
@@ -1,48 +1,60 @@
 using UnityEngine;
-
-public class RigidBodyCharacterController : MonoBehaviour
+namespace lecture20
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
-    private Rigidbody rb;
-
-    void Start()
+    public class RigidBodyCharacterController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
-        rb.mass = 1f;
-        rb.drag = 0.5f;
-        rb.angularDrag = 10f;
-        rb.useGravity = true;
-        rb.interpolation = RigidbodyInterpolation.Interpolate;
-        rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
-        rb.freezeRotation = true;
-    }
+        public float moveSpeed = 5f;
+        public float jumpForce = 5f;
+        private Rigidbody rb;
 
-    void FixedUpdate()
-    {
+        private Vector3 movementInput;
+        private bool jumpInput;
 
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-
-
-        Vector3 movement = new Vector3(moveX, 0f, moveZ);
-
-
-        if (movement.magnitude > 1)
+        void Start()
         {
-            movement.Normalize();
+
+            rb = GetComponent<Rigidbody>();
+
+
+            rb.mass = 1f;
+            rb.drag = 0.5f;
+            rb.angularDrag = 10f;
+            rb.useGravity = true;
+            rb.interpolation = RigidbodyInterpolation.Interpolate;
+            rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            rb.freezeRotation = true;
+            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
         }
 
-
-        Vector3 newVelocity = new Vector3(movement.x * moveSpeed, rb.velocity.y, movement.z * moveSpeed);
-        rb.velocity = newVelocity;
-
-
-        if (Mathf.Abs(rb.velocity.y) < 0.1f && Input.GetKey(KeyCode.Space))
+        void Update()
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+            movementInput = new Vector3(moveX, 0f, moveZ);
+
+
+            jumpInput = Input.GetKeyDown(KeyCode.Space);
+        }
+
+        void FixedUpdate()
+        {
+
+            if (movementInput.magnitude > 1f)
+            {
+                movementInput.Normalize();
+            }
+
+            Vector3 newVelocity = new Vector3(movementInput.x * moveSpeed, rb.velocity.y, movementInput.z * moveSpeed);
+            rb.velocity = newVelocity;
+
+
+            if (Mathf.Abs(rb.velocity.y) < 0.1f && jumpInput)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            }
         }
     }
+
+
 }
-
-
